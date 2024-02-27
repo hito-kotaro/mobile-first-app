a60e3e23175166c8165a3d1b141788c505702279
hito-kotaro <tohi.work@gmail.com>
2024-02-28 00:38:50 +0900

    markup memo list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,41 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Index = () => {
   return (
-    <View>
-      <Text>Hello</Text>
+    <View style={styles.container}>
+      {/*ヘッダ部分*/}
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+      {/*リスト部分*/}
+      <View>
+        {/*メモカード*/}
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2023/01/01</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Index;
